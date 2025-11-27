c3fc4814a5cb93f639ef461e84aaca35b70533ef
Charles JAVERLIAT <charles.javerliat@gmail.com>
2025-11-27 17:42:29 +0100

    Move assets

--- run_offline_demo.sh
+++ run_offline_demo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 python demo/offline/demo.py --sequence-name stone_quarry --batch-size 16 --target-fps 50 --shared-intrinsics \
-./demo/offline/stone_quarry_1.mp4 \
-./demo/offline/stone_quarry_2.mp4 \
-./demo/offline/stone_quarry_3.mp4 \
-./demo/offline/stone_quarry_4.mp4 \
-./demo/offline/stone_quarry_5.mp4 \
-./demo/offline/stone_quarry_6.mp4
+./assets/stone_quarry_1.mp4 \
+./assets/stone_quarry_2.mp4 \
+./assets/stone_quarry_3.mp4 \
+./assets/stone_quarry_4.mp4 \
+./assets/stone_quarry_5.mp4 \
+./assets/stone_quarry_6.mp4
